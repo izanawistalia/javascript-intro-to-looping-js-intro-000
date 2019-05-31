272e1d00b88a0008aca6d49cedd2d5d256df2229
rihan <dikshabelwal0@gmail.com>
2019-05-31 04:29:46 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop( myArr ){
+  for( let a=0; a<25; a++){
+
+     myArr[a]= `"I am ${a} strange loop${a === 0 ? '' : 's'}."`;
+
+  }
+  return myArr;
+}
+function whileLoop( num ){
+  while(num>0){
+    console.log(num);
+    --num;
+
+  }
+ return 'done';
+}
+var i = 0;
+
+function incrementVariable() {
+  return i++;
+}
+function doWhileLoop( num ){
+  do{
+    console.log("I run once regardless.")
+  }while(incrementVariable()<num);
+}
